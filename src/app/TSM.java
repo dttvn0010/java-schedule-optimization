fb452ff5d1bf9cb353e3d78fedc164570f1d33cd
dttvn0010 <duongthanhtungvn01@gmail.com>
2020-04-28 14:42:27 +0700

    Update

--- src/app/TSM.java
+++ src/app/TSM.java
@@ -11,87 +11,87 @@ import api.GAOptimizer.SelectionType;
 import util.CombinationUtil;
 
 public class TSM {
-	
-	private static double[][] distance_matrix = {
-			{0, 3,  5,  8, 5,  45, 23, 30},
-			{3, 0,  6,  7, 15, 43, 10, 5},
-			{5, 6,  0,  9, 12, 6,  17, 4},
-			{8, 7,  9,  0, 20, 32, 12, 26},
-			{5, 15, 12, 20, 0, 11, 24, 36},
-			{45, 43, 6, 32, 11, 0, 48, 12},
-			{23, 10, 17, 12, 24,48, 0, 41},
-			{30, 5, 4, 26, 36, 12, 41, 0}
-	};
-	
-	private final static int N = distance_matrix.length - 1;
-	
-	static class TSMChromosome extends Chromosome {
-		
-		@Override
-		protected void randomInit(Random rand) {
-			
-			encoded = CombinationUtil.genPermation(N, rand);
-			for(int i = 0; i < encoded.length; i++) {
-				encoded[i] += 1;
-			}			
-		}
-		
-		public TSMChromosome(Random rand) {
-						
-			super(rand);
-		}
-		
-		public TSMChromosome(int[] encoded) {
-			super(encoded);
-		}
-				
-		@Override
-		protected double calcFitness() {
-			double distance = 0;
-			int prev_i = 0;
-			
-			for(int i : encoded) {
-				distance += distance_matrix[prev_i][i];
-				prev_i = i;
-			}
-			
-			distance += distance_matrix[prev_i][0];
-			return 1.0/distance;
-		}
+    
+    private static double[][] distance_matrix = {
+            {0, 3,  5,  8, 5,  45, 23, 30},
+            {3, 0,  6,  7, 15, 43, 10, 5},
+            {5, 6,  0,  9, 12, 6,  17, 4},
+            {8, 7,  9,  0, 20, 32, 12, 26},
+            {5, 15, 12, 20, 0, 11, 24, 36},
+            {45, 43, 6, 32, 11, 0, 48, 12},
+            {23, 10, 17, 12, 24,48, 0, 41},
+            {30, 5, 4, 26, 36, 12, 41, 0}
+    };
+    
+    private final static int N = distance_matrix.length - 1;
+    
+    static class TSMChromosome extends Chromosome {
+        
+        @Override
+        protected void randomInit(Random rand) {
+            
+            encoded = CombinationUtil.genPermation(N, rand);
+            for(int i = 0; i < encoded.length; i++) {
+                encoded[i] += 1;
+            }            
+        }
+        
+        public TSMChromosome(Random rand) {
+                        
+            super(rand);
+        }
+        
+        public TSMChromosome(int[] encoded) {
+            super(encoded);
+        }
+                
+        @Override
+        protected double calcFitness() {
+            double distance = 0;
+            int prev_i = 0;
+            
+            for(int i : encoded) {
+                distance += distance_matrix[prev_i][i];
+                prev_i = i;
+            }
+            
+            distance += distance_matrix[prev_i][0];
+            return 1.0/distance;
+        }
 
-		@Override
-		protected Chromosome fromEncoded(int[] encoded) {
-			return new TSMChromosome(encoded);			
-		}
-		
-	}
-	
-	static class TSMGAOptimizer extends GAOptimizer {
-	
-		public TSMGAOptimizer(int populationSize, int crossOverPoolSize, double mutationRate,
-				SelectionType selectionType, CrossOverType crossOverType, MutationType mutationType, 
-				Map<String, Object> params) {
-			super(populationSize, crossOverPoolSize, mutationRate, selectionType, crossOverType, mutationType, params);
-		}
+        @Override
+        protected Chromosome fromEncoded(int[] encoded) {
+            return new TSMChromosome(encoded);            
+        }
+        
+    }
+    
+    static class TSMGAOptimizer extends GAOptimizer {
+    
+        public TSMGAOptimizer(int populationSize, int crossOverPoolSize, double mutationRate,
+                SelectionType selectionType, CrossOverType crossOverType, MutationType mutationType, 
+                Map<String, Object> params) {
+            super(populationSize, crossOverPoolSize, mutationRate, selectionType, crossOverType, mutationType, params);
+        }
 
-		@Override
-		protected Chromosome newRandomChromosome(Random rand) {
-			return new TSMChromosome(rand);
-		}
+        @Override
+        protected Chromosome newRandomChromosome(Random rand) {
+            return new TSMChromosome(rand);
+        }
 
-			
-	}
-	
-	public static void main(String[] args) {
-		Map<String, Object> params = new HashMap<>();
-		params.put("bitMutationRate", 0.2);
-		
-		GAOptimizer gaOptimizer = new TSMGAOptimizer(40, 20, 0.05, 
-										SelectionType.ROULETTE,
-										CrossOverType.UNIFORM, 
-										MutationType.SWITCH_POINT, 
-										params);
-		gaOptimizer.run(500);
-	}
+            
+    }
+    
+    public static void main(String[] args) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("bitMutationRate", 0.2);
+        
+        GAOptimizer gaOptimizer = new TSMGAOptimizer(40, 20, 0.05, 
+                                        SelectionType.ROULETTE,
+                                        CrossOverType.UNIFORM, 
+                                        MutationType.SWITCH_POINT, 
+                                        params);
+        gaOptimizer.run(500);
+    }
 
 }
--- src/util/CombinationUtil.java
+++ src/util/CombinationUtil.java
@@ -6,80 +6,80 @@ import java.util.Random;
 
 public class CombinationUtil {
 
-	public static int[] genPermation(int N) {
-		return genPermation(N, null, null);
-	}
-	
-	public static int[] genPermation(int N, Random rand) {
-		
-		return genPermation(N, null, rand);
-	}
-	
-	public static int[] genPermation(int N, int[] fixedIndexes) {
-		return genPermation(N, fixedIndexes, null);		
-	}
-	
-	public static int[] genPermation(int N, int[] fixedIndexes, Random rand) {
-		return genCombination(N, N, fixedIndexes, rand);
-	}
-	
-	public static int[] genCombination(int N, int k) {
-		return genCombination(N, k, null, null);
-	}
-	
-	public static int[] genCombination(int N, int k, Random rand) {
-		
-		return genCombination(N, k, null, rand);
-	}
-	
-	public static int[] genCombination(int N, int k, int[] fixedIndexes) {
-		return genCombination(N, k, fixedIndexes, null);		
-	}
-	
-	public static int[] genCombination(int N, int k, int[] fixedIndexes, Random rand) {
-		
-		if(fixedIndexes == null) {
-			fixedIndexes = new int[k];
-			for(int i = 0; i < k; i++) {
-				fixedIndexes[i] = -1;
-			}
-		}
-		
-		List<Integer> indexes = new ArrayList<>();
-		for(int i = 0; i < N; i++) {
-			indexes.add(i);
-		}
-		
-		for(int index : fixedIndexes) {
-			if(index >= 0) {
-				indexes.remove((Integer) index);
-			}
-		}
-		
-		if(rand == null) rand = new Random();
-		
-		int[] result = new int[k];
-		
-		for(int i = 0; i < k; i++) {
-			if(fixedIndexes[i] >= 0) {
-				result[i] = fixedIndexes[i];
-			}else {
-				int r = (int) (rand.nextDouble() * indexes.size());
-				result[i] = indexes.get(r);
-				indexes.remove(r);
-			}
-		}
-		
-		return result;
-	}
-	
-	public static void main(String[] args) {
-		int[] fixedIndexes =  {-1,-1, 2, 5, -1, 0, 6, -1, -1};
-		
-		for(int k = 0; k < 10; k++) {
-			int[] indexes = genPermation(9, fixedIndexes);
-			for(int index : indexes) System.out.print(index + " ");
-			System.out.println();
-		}
-	}
+    public static int[] genPermation(int N) {
+        return genPermation(N, null, null);
+    }
+    
+    public static int[] genPermation(int N, Random rand) {
+        
+        return genPermation(N, null, rand);
+    }
+    
+    public static int[] genPermation(int N, int[] fixedIndexes) {
+        return genPermation(N, fixedIndexes, null);        
+    }
+    
+    public static int[] genPermation(int N, int[] fixedIndexes, Random rand) {
+        return genCombination(N, N, fixedIndexes, rand);
+    }
+    
+    public static int[] genCombination(int N, int k) {
+        return genCombination(N, k, null, null);
+    }
+    
+    public static int[] genCombination(int N, int k, Random rand) {
+        
+        return genCombination(N, k, null, rand);
+    }
+    
+    public static int[] genCombination(int N, int k, int[] fixedIndexes) {
+        return genCombination(N, k, fixedIndexes, null);        
+    }
+    
+    public static int[] genCombination(int N, int k, int[] fixedIndexes, Random rand) {
+        
+        if(fixedIndexes == null) {
+            fixedIndexes = new int[k];
+            for(int i = 0; i < k; i++) {
+                fixedIndexes[i] = -1;
+            }
+        }
+        
+        List<Integer> indexes = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            indexes.add(i);
+        }
+        
+        for(int index : fixedIndexes) {
+            if(index >= 0) {
+                indexes.remove((Integer) index);
+            }
+        }
+        
+        if(rand == null) rand = new Random();
+        
+        int[] result = new int[k];
+        
+        for(int i = 0; i < k; i++) {
+            if(fixedIndexes[i] >= 0) {
+                result[i] = fixedIndexes[i];
+            }else {
+                int r = (int) (rand.nextDouble() * indexes.size());
+                result[i] = indexes.get(r);
+                indexes.remove(r);
+            }
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        int[] fixedIndexes =  {-1,-1, 2, 5, -1, 0, 6, -1, -1};
+        
+        for(int k = 0; k < 10; k++) {
+            int[] indexes = genPermation(9, fixedIndexes);
+            for(int index : indexes) System.out.print(index + " ");
+            System.out.println();
+        }
+    }
 }
